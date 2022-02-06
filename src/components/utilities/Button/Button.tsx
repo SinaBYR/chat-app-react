c814403ae06805d1e2474199efca83b8efd0ff92
SinaBYR <sina.beyraghdar1@gmail.com>
2022-02-06 18:44:23 +0330

    Create utility button component

---
+++ src/components/utilities/Button/Button.tsx
@@ -0,0 +1,31 @@
+import * as React from "react"
+import styled, { css } from "styled-components"
+
+type StyledProps = {
+  bgColor: 'orange' | 'black' | 'white';
+  color: 'orange' | 'black' | 'white';
+  fullWidth?: boolean
+}
+
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & StyledProps
+
+const ButtonStyled = styled.button<StyledProps>`
+  display: block;
+  ${({ fullWidth }) => fullWidth && css`
+    width: 100%;
+  `}
+  color: ${({theme, color}) => theme.colors[color]};
+  background-color: ${({theme, bgColor}) => theme.colors[bgColor]};
+  font-size: 1.25rem;
+  padding: 0.75rem 1rem;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: transform 400ms;
+
+  &:active {
+    transform: scale(0.97);
+  }
+`
+
+export const Button = ({ children, ...props } : ButtonProps) => <ButtonStyled {...props}>{children}</ButtonStyled>
