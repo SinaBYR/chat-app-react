f3b4befc69bbbf26a248d4f9702f42b1f2683bcb
SinaBYR <sina.beyraghdar1@gmail.com>
2022-02-13 13:19:09 +0330

    Fix props - Add loading state test

--- src/components/Showcase/Auth/SignUp/SignUp.test.tsx
+++ src/components/Showcase/Auth/SignUp/SignUp.test.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from "../../../../theme/ThemeProvider"
 import { SignUp } from "./SignUp"
 
 test('renders labels and inputs', () => {
-  const { container } = render(<ThemeProvider><SignUp /></ThemeProvider>)
+  const { container } = render(<ThemeProvider><SignUp loading={false}/></ThemeProvider>)
   const labels = container.getElementsByTagName('label')
   const inputs = container.getElementsByTagName('input')
 
@@ -12,7 +12,7 @@ test('renders labels and inputs', () => {
 })
 
 test('email input', () => {
-  render(<ThemeProvider><SignUp /></ThemeProvider>)
+  render(<ThemeProvider><SignUp loading={false}/></ThemeProvider>)
   const input = screen.getByTestId('email')
 
   expect(input).toHaveAttribute('type', 'email')
@@ -21,7 +21,7 @@ test('email input', () => {
 })
 
 test('password input', () => {
-  render(<ThemeProvider><SignUp /></ThemeProvider>)
+  render(<ThemeProvider><SignUp loading={false}/></ThemeProvider>)
   const input = screen.getByTestId('password')
 
   expect(input).toHaveAttribute('type', 'password')
@@ -30,7 +30,7 @@ test('password input', () => {
 })
 
 test('confirm input', () => {
-  render(<ThemeProvider><SignUp /></ThemeProvider>)
+  render(<ThemeProvider><SignUp loading={false}/></ThemeProvider>)
   const input = screen.getByTestId('confirm')
   
   expect(input).toHaveAttribute('type', 'password')
@@ -39,9 +39,17 @@ test('confirm input', () => {
 })
 
 test('submit button', () => {
-  render(<ThemeProvider><SignUp /></ThemeProvider>)
+  render(<ThemeProvider><SignUp loading={false}/></ThemeProvider>)
   const button = screen.getByRole('button')
 
   expect(button).toHaveTextContent('Sign up')
+  expect(button).toHaveAttribute('type', 'submit')
+})
+
+test('submit button has loading', () => {
+  render(<ThemeProvider><SignUp loading={true}/></ThemeProvider>)
+  const button = screen.getByRole('button')
+
+  expect(button).toHaveTextContent('Loading')
   expect(button).toHaveAttribute('type', 'submit')
 })
