f048eef3ce1d9c0a4e30cc890a36553cc1a95064
SinaBYR <sina.beyraghdar1@gmail.com>
2022-02-15 14:29:21 +0330

    Add showcase test

--- src/components/Showcase/Showcase.test.tsx
+++ src/components/Showcase/Showcase.test.tsx
@@ -1,2 +1,10 @@
 import { render, screen } from '@testing-library/react'
+import { ThemeProvider } from '../../theme/ThemeProvider'
+import { Showcase } from './Showcase'
 
+test('renders showcase with styles', () => {
+  const { container } = render(<ThemeProvider><Showcase /></ThemeProvider>)
+
+  const mainTag = container.getElementsByTagName('main')
+  expect(mainTag[0]).toBeInTheDocument()
+})
