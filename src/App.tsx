fdbd567b947c767c7a1c416a8e3ecc128cabb5ae
SinaBYR <sina.beyraghdar1@gmail.com>
2022-02-15 17:00:39 +0330

    Change css flex styles and Add Footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
 import styled from "styled-components"
+import { Footer } from "./components/Footer/Footer"
 import { Showcase } from "./components/Showcase/Showcase"
-import { ThemeProvider } from "styled-components"
-import { theme } from "./theme/theme"
+import { ThemeProvider } from "./theme/ThemeProvider"
 
 const AppStyled = styled.div`
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
+  justify-content: center;
   align-items: center;
   height: 100%;
   min-height: 100vh;
-  color: ${(props) => props.theme.colors.orange};
   background-color: ${(props) => props.theme.colors.black};
 `
 
 export function App() {
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider>
       <AppStyled>
         <Showcase />
+        <Footer />
       </AppStyled>
       {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
         <path fill="#0099ff" fill-opacity="1" d="M0,128L48,138.7C96,149,192,171,288,186.7C384,203,480,213,576,213.3C672,213,768,203,864,181.3C960,160,1056,128,1152,122.7C1248,117,1344,139,1392,149.3L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
