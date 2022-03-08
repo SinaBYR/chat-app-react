cd79233891fc2efd6f88786ab46344c6e7f6a8ae
SinaBYR <sina.beyraghdar1@gmail.com>
2022-03-08 12:50:13 +0330

    Finish Header element (user icon and username)

--- src/components/Dashboard/Header/Sidedrawer/Sidedrawer.tsx
+++ src/components/Dashboard/Header/Sidedrawer/Sidedrawer.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { FaUserCircle } from 'react-icons/fa'
 
 const Wrapper = styled.div`
   position: absolute;
@@ -7,16 +8,29 @@ const Wrapper = styled.div`
   width: 250px;
   height: 100%;
   z-index: 1;
-  /* border: 1px solid red; */
-  padding: 1rem;
+  /* border: 1px solid ${({ theme }) => theme.colors.black}; */
+  /* padding: 1rem; */
   background-color: ${({ theme }) => theme.colors.black};
-  color: ${({ theme }) => theme.colors.orange};
-  box-shadow: 0 0 8px 4px #15171a;
+  /* color: ${({ theme }) => theme.colors.orange}; */
+  /* box-shadow: 0 0 8px 4px #15171a; */
 `
 
 const Header = styled.header`
-  
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 20%;
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.orange};
 `
+
+const UserIcon = styled(FaUserCircle)`
+  font-size: 3rem;
+  margin-bottom: 1rem;
+`
+
+const Username = styled.h4``
 
 const Main = styled.main`
   
@@ -30,7 +44,8 @@ export function Sidedrawer() {
   return (
     <Wrapper>
       <Header>
-
+        <UserIcon />
+        <Username>ThisIsKingdomeCome</Username>
       </Header>
       <Main>
 
