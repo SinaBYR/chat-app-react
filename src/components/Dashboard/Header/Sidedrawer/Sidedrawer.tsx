f5604c925aff54abc6120a633666cf563c38b06c
SinaBYR <sina.beyraghdar1@gmail.com>
2022-03-08 13:42:11 +0330

    Finish sidedrawer styles without clean up

--- src/components/Dashboard/Header/Sidedrawer/Sidedrawer.tsx
+++ src/components/Dashboard/Header/Sidedrawer/Sidedrawer.tsx
@@ -1,10 +1,16 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import { FaUserCircle } from 'react-icons/fa'
+import { IoIosCreate } from 'react-icons/io'
+import { IoCreateOutline } from 'react-icons/io5'
+import { MdTravelExplore, MdLogout } from 'react-icons/md'
+import { CgProfile } from 'react-icons/cg'
 
 const Wrapper = styled.div`
   position: absolute;
   top: 0;
   right: 0;
+  display: flex;
+  flex-direction: column;
   width: 250px;
   height: 100%;
   z-index: 1;
@@ -21,6 +27,7 @@ const Header = styled.header`
   justify-content: center;
   align-items: center;
   height: 20%;
+  height: 40%;
   width: 100%;
   background-color: ${({ theme }) => theme.colors.orange};
 `
@@ -33,11 +40,57 @@ const UserIcon = styled(FaUserCircle)`
 const Username = styled.h4``
 
 const Main = styled.main`
+  height: 100%;
+  width: 100%;
+  /* background-color: red; */
+`
+
+const Button = styled.button`
+  position: relative;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  font-size: 1rem;
+  padding: 1.5rem 1rem;
+  border: none;
+  border-radius: 2px;
+  cursor: pointer;
+  overflow: hidden;
+  background-color: unset;
+  color: ${({ theme }) => theme.colors.orange};
+  background-color: #15171a;
+  border-bottom: 1px solid ${({ theme }) => theme.colors.black};
+  text-align: left;
   
+  &:active {
+    /* opacity: 0.8; */
+    background-color: #e00000;
+    background-color: #1f2227;
+  }
+`
+
+const CreateIcon = styled(IoCreateOutline)`
+  margin-right: 0.5rem;
+`
+
+const ExploreIcon = styled(MdTravelExplore)`
+  margin-right: 0.5rem;
+`
+
+const ProfileIcon = styled(CgProfile)`
+  margin-right: 0.5rem;
+`
+
+const LogoutIcon = styled(MdLogout)`
+  margin-right: 0.5rem;
 `
 
 const Footer = styled.footer`
-  
+  color: ${({ theme }) => theme.colors.orange};
+  font-size: 0.8rem;
+  padding-bottom: 1rem;
+  margin-top: auto;
+  text-align: center;
 `
 
 export function Sidedrawer() {
@@ -48,8 +101,15 @@ export function Sidedrawer() {
         <Username>ThisIsKingdomeCome</Username>
       </Header>
       <Main>
+        <Button><CreateIcon /> Create</Button>
+        <Button><ExploreIcon /> Explore channels</Button>
+        <Button><ProfileIcon /> Profile</Button>
+        <Button style={{backgroundColor: '#ff3232', color: 'white'}}><LogoutIcon /> Logout</Button>
 
       </Main>
+      <Footer>
+        Designed and developed by Sina Beyraghdar &copy; {new Date().getFullYear()}
+      </Footer>
     </Wrapper>
   )
 }
