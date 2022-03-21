c26ca7774e7511bd90d2343804be5861d29d3ad0
SinaBYR <sina.beyraghdar1@gmail.com>
2022-03-21 16:16:24 +0330

    Store session after signing up/in

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
+import { GoTrueClient, Session } from "@supabase/supabase-js"
+import { useEffect, useState } from "react"
 import styled from "styled-components"
 import { Dashboard } from "./components/Dashboard/Dashboard"
 import { Footer } from "./components/Footer/Footer"
 import { Showcase } from "./components/Showcase/Showcase"
+import { supabase } from "./supabase/supabase"
 import { ThemeProvider } from "./theme/ThemeProvider"
 
 const AppStyled = styled.div`
-  /* display: flex; */
   display: grid;
   grid-template-rows: 95% 5%;
-  /* flex-direction: column; */
-  /* justify-content: center; */
   align-items: center;
   height: 100%;
   min-height: 100vh;
@@ -21,12 +21,25 @@ const AppStyled = styled.div`
 `
 
 export function App() {
+  const [session, setSession] = useState<Session | null>(null)
+
+  useEffect(() => {
+    setSession(supabase.auth.session())
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
+  console.log(session)
+
   return (
     <ThemeProvider>
       <AppStyled>
+        {session ? <Dashboard /> : <Showcase />}
         {/* <Showcase /> */}
-        <Dashboard />
-        {/* <Footer /> */}
+        {/* <Dashboard /> */}
+        <Footer />
       </AppStyled>
       {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
         <path fill="#0099ff" fill-opacity="1" d="M0,128L48,138.7C96,149,192,171,288,186.7C384,203,480,213,576,213.3C672,213,768,203,864,181.3C960,160,1056,128,1152,122.7C1248,117,1344,139,1392,149.3L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
