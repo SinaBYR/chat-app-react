6ad15b58426baacf677b50dfd308832ed8c0b1bc
SinaBYR <sina.beyraghdar1@gmail.com>
2022-03-21 12:04:00 +0330

    Change styles code

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,16 +1,26 @@
 import styled from 'styled-components'
+import { DesktopChannels } from './Channels/DesktopChannels/DesktopChannels'
 import { Feed } from './Feed/Feed'
 import { Header } from './Header/Header'
 
 const Wrapper = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
   width: 100%;
   height: 100%;
+  padding: 1rem;
+
+  @media (max-width: 30em) {
+    padding: 0;
+  }
 `
 
 export function Dashboard() {
   return (
     <Wrapper>
       <Header />
+      <DesktopChannels />
       {/* <Feed /> */}
     </Wrapper>
   )
