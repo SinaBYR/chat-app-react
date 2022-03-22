67a3c8c7fb48a0af298896871ba075991ebc2c7b
SinaBYR <sina.beyraghdar1@gmail.com>
2022-03-22 19:25:56 +0430

    Move ThemeProvider a level up in the component tree

--- src/App.tsx
+++ src/App.tsx
@@ -34,16 +34,9 @@ export function App() {
   console.log(session)
 
   return (
-    <ThemeProvider>
-      <AppStyled>
-        {session ? <Dashboard /> : <Showcase />}
-        {/* <Showcase /> */}
-        {/* <Dashboard /> */}
-        <Footer />
-      </AppStyled>
-      {/* <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-        <path fill="#0099ff" fill-opacity="1" d="M0,128L48,138.7C96,149,192,171,288,186.7C384,203,480,213,576,213.3C672,213,768,203,864,181.3C960,160,1056,128,1152,122.7C1248,117,1344,139,1392,149.3L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
-      </svg> */}
-    </ThemeProvider>
+    <AppStyled>
+      {session ? <Dashboard /> : <Showcase />}
+      <Footer />
+    </AppStyled>
   )
 }
