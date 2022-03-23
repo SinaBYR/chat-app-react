516967eea88920216d911b97006fe384587c6a66
SinaBYR <sina.beyraghdar1@gmail.com>
2022-03-23 18:53:32 +0430

    Add logging out logic

--- src/components/Dashboard/Header/User/DesktopUser/DesktopUser.tsx
+++ src/components/Dashboard/Header/User/DesktopUser/DesktopUser.tsx
@@ -1,5 +1,8 @@
 import { ArrowDownIcon, ArrowUpIcon, Lower, PersonIcon, Upper, Username, Wrapper } from './DesktopUserStyled'
 import { Button } from '../../../../utilities'
+import { supabase } from '../../../../../supabase/supabase'
+import { useContext } from 'react'
+import { DispatchContext, InitialState, StateContext } from '../../../../../store/context'
 
 interface Props {
   extended: boolean
@@ -7,6 +10,18 @@ interface Props {
 }
 
 export function DesktopUser({ extended, toggleDropdown } : Props) {
+  const { auth } = useContext(StateContext) as InitialState
+  const dispatch = useContext(DispatchContext)
+
+  async function signoutHandler() {
+    try {
+      await supabase.auth.signOut()
+      dispatch({ type: 'SIGN_OUT' })
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
   return (
     <Wrapper>
       <Upper onClick={toggleDropdown}>
@@ -18,7 +33,7 @@ export function DesktopUser({ extended, toggleDropdown } : Props) {
         extended && (
         <Lower>
           <Button style={{border: '1px solid #1A1C20'}} bgColor="orange" foreColor="black" fullWidth>Profile</Button>
-          <Button bgColor="red" foreColor="white" fullWidth>Logout</Button>
+          <Button bgColor="red" foreColor="white" fullWidth onClick={signoutHandler}>Logout</Button>
         </Lower>
         )
       }
