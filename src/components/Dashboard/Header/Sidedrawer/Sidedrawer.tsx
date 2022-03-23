a8ad58ebde6cc00975c157f73a59c35911852aee
SinaBYR <sina.beyraghdar1@gmail.com>
2022-03-23 18:57:20 +0430

    Add logging out logic

--- src/components/Dashboard/Header/Sidedrawer/Sidedrawer.tsx
+++ src/components/Dashboard/Header/Sidedrawer/Sidedrawer.tsx
@@ -1,6 +1,21 @@
+import { useContext } from "react";
+import { DispatchContext, InitialState, StateContext } from "../../../../store/context";
+import { supabase } from "../../../../supabase/supabase";
 import { Header, Button, CreateIcon, ExploreIcon, Footer, Main, ProfileIcon, UserIcon, Username, Wrapper, LogoutIcon } from "./SidedrawerStyled";
 
 export function Sidedrawer() {
+  const { auth } = useContext(StateContext) as InitialState
+  const dispatch = useContext(DispatchContext)
+
+  async function signoutHandler() {
+    try {
+      await supabase.auth.signOut()
+      dispatch({ type: 'SIGN_OUT' })
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
   return (
     <Wrapper>
       <Header>
@@ -11,7 +26,7 @@ export function Sidedrawer() {
         <Button><CreateIcon /> Create</Button>
         <Button><ExploreIcon /> Explore channels</Button>
         <Button><ProfileIcon /> Profile</Button>
-        <Button style={{backgroundColor: '#ff3232', color: 'white'}}><LogoutIcon /> Logout</Button>
+        <Button style={{backgroundColor: '#ff3232', color: 'white'}} onClick={signoutHandler}><LogoutIcon /> Logout</Button>
       </Main>
       <Footer>
         Designed and developed by Sina Beyraghdar &copy; {new Date().getFullYear()}
