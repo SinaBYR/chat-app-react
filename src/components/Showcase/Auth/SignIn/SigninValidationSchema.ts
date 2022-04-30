64c18efc37cc11c96c25b24f024cecdaa36c5d46
SinaBYR <sina.beyraghdar1@gmail.com>
2022-04-30 18:26:59 +0430

    Deleted files commit

--- src/components/Showcase/Auth/SignIn/SigninValidationSchema.ts
+++
@@ -1,6 +0,0 @@
-import * as yup from 'yup'
-
-export const SigninValidationSchema = yup.object().shape({
-  email: yup.string().required('Email is a required field'),
-  password: yup.string().required('Password is a required field')
-})
--- src/components/Showcase/Landing/Landing.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react"
-import { ThemeProvider } from "../../../theme/ThemeProvider"
-import { Landing } from "./Landing"
-
-test('render chat app heading', () => {
-  render(<ThemeProvider><Landing /></ThemeProvider>)
-  const chatHeading = screen.getByText('Chat App')
-  expect(chatHeading).toBeInTheDocument()
-})
