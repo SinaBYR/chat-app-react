a5a259ff9c36ab2fa98afcd6718ac034da4b2fef
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2022-08-09 16:13:27 +0430

    Fix spinner visual bug

--- src/components/utilities/Spinner/Spinner.tsx
+++ src/components/utilities/Spinner/Spinner.tsx
@@ -3,11 +3,9 @@ import styled, { keyframes } from 'styled-components';
 
 const animation = keyframes`
   0% {
-    -webkit-transform: rotate(0deg);
     transform: rotate(0deg);
   }
   100% {
-    -webkit-transform: rotate(360deg);
     transform: rotate(360deg);
   }
 `;
@@ -20,26 +18,21 @@ const Wrapper = styled.div`
   margin: 0;
   font-size: 6px;
   position: relative;
-  text-indent: -9999em;
   border-top: 0.6em solid rgba(255, 255, 255, 0.2);
   border-right: 0.6em solid rgba(255, 255, 255, 0.2);
   border-bottom: 0.6em solid rgba(255, 255, 255, 0.2);
   border-left: 0.6em solid #ffffff;
-  -webkit-transform: translateZ(0);
-  -ms-transform: translateZ(0);
-  transform: translateZ(0);
-  -webkit-animation: load8 1.1s infinite linear;
   animation: ${animation} 1.1s infinite linear;
 
   &::after {
-    border-radius: 50%;
     width: 2.5em;
     height: 2.5em;
+    border-radius: 50%;
   }
 `;
 
 export function Spinner(props: HTMLAttributes<HTMLDivElement>) {
   return (
-    <Wrapper {...props}>Loading...</Wrapper>
+    <Wrapper {...props} />
   )
 }
