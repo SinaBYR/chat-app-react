279c2909e0e4e486e51bd423d7293b60e29ef5ef
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2022-08-18 11:14:46 +0430

    Wrap rendered component inside AppContextProvider to be able to use useSelect custom hook

--- src/components/Dashboard/Header/Header.test.tsx
+++ src/components/Dashboard/Header/Header.test.tsx
@@ -1,18 +1,21 @@
 import { fireEvent, render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import { AppContextProvider } from "../../../store/context";
 import { ThemeProvider } from "../../../theme/ThemeProvider";
 import { Header } from "./Header";
 
 test('renders logo title', () => {
-  const { container } = render(<ThemeProvider><Header /></ThemeProvider>)
+  // Wrap renderd component inside AppContextProvider to be able to use useSelect custom hook
+  const { container } = render(<AppContextProvider><ThemeProvider><MemoryRouter><Header /></MemoryRouter></ThemeProvider></AppContextProvider>)
 
   const heading = container.getElementsByTagName('h1')[0]
   expect(heading).toHaveTextContent('Chat App')
 })
 
 test('renders 2 header buttons', () => {
-  const { container } = render(<ThemeProvider><Header /></ThemeProvider>)
+  const { container } = render(<AppContextProvider><ThemeProvider><MemoryRouter><Header /></MemoryRouter></ThemeProvider></AppContextProvider>)
 
   const buttonsWrapper = container.getElementsByTagName('ul')
-  const buttons = buttonsWrapper[0].getElementsByTagName('button')
+  const buttons = buttonsWrapper[0].getElementsByTagName('a')
   expect(buttons.length).toEqual(2)
 })
--- src/components/Dashboard/Header/User/DesktopUser/DesktopUser.test.tsx
+++ src/components/Dashboard/Header/User/DesktopUser/DesktopUser.test.tsx
@@ -1,11 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import { ThemeProvider } from "../../../../../theme/ThemeProvider";
 import { DesktopUser } from "./DesktopUser";
+import { AppContextProvider } from "../../../../../store/context";
 
 const onClickMock = jest.fn()
 
 test('renders desktop unextended user', () => {
-  const { container } = render(<ThemeProvider><DesktopUser extended={false} toggleDropdown={onClickMock}/></ThemeProvider>)
+  const { container } = render(<AppContextProvider><ThemeProvider><DesktopUser extended={false} toggleDropdown={onClickMock}/></ThemeProvider></AppContextProvider>)
 
   const userButton = container.getElementsByTagName('svg')[0]
 
@@ -14,7 +15,7 @@ test('renders desktop unextended user', () => {
 })
 
 test('renders desktop extended user', () => {
-  const { container } = render(<ThemeProvider><DesktopUser extended={true} toggleDropdown={onClickMock}/></ThemeProvider>)
+  const { container } = render(<AppContextProvider><ThemeProvider><DesktopUser extended={true} toggleDropdown={onClickMock}/></ThemeProvider></AppContextProvider>)
 
   const logoutButton = screen.getByText('Logout')
   const profileButton = screen.getByText('Profile')
