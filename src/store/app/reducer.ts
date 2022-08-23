5f77ed0c777a88bad1653be883ba3517e06dfe49
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2022-08-23 14:56:29 +0430

    Add STORE_CHANNELS case to switch statement

--- src/store/app/reducer.ts
+++ src/store/app/reducer.ts
@@ -2,16 +2,26 @@ import { AppActionTypes } from './actions';
 import { AppState } from './types'
 
 export const appInitialState: AppState = {
-  firstTime: true
+  firstTime: true,
+  channels: null
 }
 
 export function appReducer(state: AppState = appInitialState, action: AppActionTypes): AppState {
   switch(action.type) {
     case 'SET_FIRST_TIME_TO_FALSE': {
       return {
+        ...state,
         firstTime: false
       }
-    }
+    };
+
+    case 'STORE_CHANNELS': {
+      return {
+        ...state,
+        channels: action.payload
+      }
+    };
+
     default: {
       return state
     }
