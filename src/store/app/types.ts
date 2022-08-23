0ab8b65e05195c9a84e51ce73c8aa4d0fd915834
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2022-08-23 14:54:40 +0430

    Add Channel custom type

--- src/store/app/types.ts
+++ src/store/app/types.ts
@@ -1,3 +1,12 @@
+export interface Channel {
+  id: string;
+  name: string;
+  owner_id: string;
+  created_at: string;
+  joined_at: string;
+};
+
 export interface AppState {
-  firstTime: boolean
-}
+  firstTime: boolean;
+  channels: Channel[] | null
+};
