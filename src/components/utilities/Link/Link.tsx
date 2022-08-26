af0fee7b74e4d605e50ac606daf17998a096fa7b
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2022-08-26 18:42:13 +0430

    Add Navlink feature to custom Link component

--- src/components/utilities/Link/Link.tsx
+++ src/components/utilities/Link/Link.tsx
@@ -1,16 +1,17 @@
 import styled, { css } from "styled-components";
-import { Link as RouterLink, LinkProps as RouterLinkProps } from 'react-router-dom';
+import { Link as RouterLink, NavLink as RouterNavLink, LinkProps as RouterLinkProps } from 'react-router-dom';
 
 type StyledProps = {
   // Each custom prop is optional, because they've got default value.
   $bgColor?: 'orange' | 'black' | 'white' | 'red';
   $foreColor?: 'orange' | 'black' | 'white';
-  $fullWidth?: boolean
+  $fullWidth?: boolean;
+  $navLink?: boolean;
 };
 
 type LinkProps = RouterLinkProps & StyledProps;
 
-const LinkWrapper = styled(RouterLink)<StyledProps >`
+const LinkWrapper = styled(RouterLink)<StyledProps>`
   display: flex;
   align-items: center;
   ${({ $fullWidth }) => $fullWidth && css`
@@ -32,6 +33,33 @@ const LinkWrapper = styled(RouterLink)<StyledProps >`
   }
 `;
 
+const NavLinkWrapper = styled(RouterNavLink)<StyledProps>`
+  display: flex;
+  align-items: center;
+  ${({ $fullWidth }) => $fullWidth && css`
+    width: 100%;
+  `};
+  color: ${({theme, $foreColor}) => $foreColor && theme.colors[$foreColor]};
+  background-color: ${({theme, $bgColor}) => $bgColor && theme.colors[$bgColor]};
+  font-size: 1rem;
+  padding: 0.65rem 1rem;
+  border: none;
+  border-radius: 2px;
+  cursor: pointer;
+  text-decoration: none;
+
+  &:disabled {
+    cursor: not-allowed;
+    transform: none;
+    opacity: 0.6;
+  }
+
+  &.active {
+    color: ${({ theme }) => theme.colors.black};
+    background-color: ${({ theme }) => theme.colors.orange} !important;
+  }
+`;
+
 // Internally, React Router's Link passes all its props to an <a> DOM element. Except the ones used by Link, like "to".
 // So the styles work because the props are interpreted by Styled Components, but then the same prop is passed again to the inner <a>, which triggers the (harmless) error message.
 // Transient props are a new pattern to pass props that are explicitly consumed only by styled components and are not meant to be passed down to deeper component layers.
@@ -42,8 +70,13 @@ export function Link({
   $foreColor = "black",
   $bgColor = "orange",
   $fullWidth,
+  $navLink = false,
   ...props
 }: LinkProps) {
+  if($navLink) {
+    return <NavLinkWrapper $foreColor={$foreColor} $bgColor={$bgColor} $fullWidth={$fullWidth} activeClassName="active" {...props}>{children}</NavLinkWrapper>
+  }
+
   return (
     <LinkWrapper $foreColor={$foreColor} $bgColor={$bgColor} $fullWidth={$fullWidth} {...props}>{children}</LinkWrapper>
   )
