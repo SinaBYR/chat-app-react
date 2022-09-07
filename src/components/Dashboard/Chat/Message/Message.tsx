920a06434f89e544ca1e843d10f8abd28802867c
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2022-09-07 16:10:20 +0430

    Create unofficial Message component

---
+++ src/components/Dashboard/Chat/Message/Message.tsx
@@ -0,0 +1,17 @@
+import { Text } from "../../../utilities";
+import { Wrapper, Time, Username } from "./MessageStyled";
+
+interface Props {
+  text: string;
+  me: boolean;
+}
+
+export function Message({ text, me }: Props) {
+  return (
+    <Wrapper me={me}>
+      <Username me={me}>Boogyman_coming_back</Username>
+      <Text style={{ fontSize: '1rem' }}>{text}</Text>
+      <Time me={me}>7:23 PM</Time>
+    </Wrapper>
+  )
+}
---
+++ src/components/Dashboard/Chat/Message/MessageStyled.tsx
@@ -0,0 +1,56 @@
+import styled, { css } from "styled-components";
+
+interface Props {
+  me: boolean;
+}
+
+export const Username = styled.div<Props>`
+  position: absolute;
+  top: -25px;
+  left: 0;
+  ${({ me }) => me && css`
+    display: none;
+  `}
+  width: 180px;
+  color: #bbb0b0;
+  margin-right: 1rem;
+  overflow-x: hidden;
+  text-overflow: ellipsis;
+`;
+
+export const Time = styled.div<Props>`
+  position: absolute;
+  right: 5px;
+  bottom: 5px;
+  color: ${({ theme }) => theme.colors.white};
+  font-size: 12px;
+  font-style: italic;
+  opacity: 0.7;
+
+  ${({ me }) => me && css`
+    left: auto;
+    color: ${({ theme }) => theme.colors.black};
+    opacity: 0.9;
+  `}
+`;
+
+export const Wrapper = styled.div<Props>`
+  position: relative;
+  align-self: flex-start;
+  max-width: 500px;
+  min-width: 100px;
+  color: ${({ theme }) => theme.colors.white};
+  background-color: #2b2e35;
+  ${({ me }) => me && css`
+    align-self: flex-end;
+    color: ${({ theme }) => theme.colors.black};
+    background-color: ${({ theme }) => theme.colors.orange};
+  `}
+  padding: 0.75rem;
+  padding-bottom: 1.5rem;
+  margin-top: 1.25rem;
+  margin-bottom: 1.5rem;
+  box-shadow: 0 0 4px black;
+  border-radius: 4px;
+  white-space: pre-wrap;
+`;
