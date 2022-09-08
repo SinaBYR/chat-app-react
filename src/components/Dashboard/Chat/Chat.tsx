549572ff99c7b4b17980223765ee77b47f1c18e5
Sina Beyraghdar <sina.beyraghdar1@gmail.com>
2022-09-08 13:19:55 +0430

    Add textarea event handlers

--- src/components/Dashboard/Chat/Chat.tsx
+++ src/components/Dashboard/Chat/Chat.tsx
@@ -1,46 +1,7 @@
-import styled from 'styled-components';
-import { useState } from 'react';
+import { ChatBox, Wrapper } from './ChatStyled';
+import { ChangeEvent, KeyboardEvent, useState } from 'react';
 import { Message } from './Message/Message';
-
-const Wrapper = styled.div`
-  position: absolute;
-  bottom: 16px;
-  left: 332px;
-  width: calc(100% - 348px);
-  height: calc(100% - 118px);
-  color: ${({ theme }) => theme.colors.white};
-  background-color: #15171a;
-  padding: 1rem;
-`;
-
-const ChatBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 90%;
-  color: black;
-  background-color: #15171a;
-  /* background: url('/img/paper.jpg') center center/cover; */
-  padding: 1rem;
-  overflow-y: auto;
-`;
-
-const InputBox = styled.textarea`
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 10%;
-  color: gray;
-  /* background-color: black; */
-  padding: 1rem;
-  user-select: text;
-  white-space: pre-wrap;
-  word-break: break-word;
-`;
+import { InputBox } from './InputBox/InputBox';
 
 export function Chat() {
   const [chats, setChats] = useState<any[]>([
@@ -49,6 +10,22 @@ export function Chat() {
     'sajhd\nJDjs'
   ]);
   const [message, setMessage] = useState<string>('');
+  
+
+  function keyPressHandler(e: KeyboardEvent<HTMLTextAreaElement>) {
+    if(e.code === 'Enter' && e.ctrlKey) {
+      const newChats = [...chats];
+      newChats.push(message.replace(/^\s+/, '').replace(/\s+$/, ''));
+      setChats(newChats);
+      setMessage('');
+      e.currentTarget.style.height = "40px";
+      e.preventDefault();
+    }
+  }
+
+  function changeHandler(e: ChangeEvent<HTMLTextAreaElement>) {
+    setMessage(e.target.value);
+  }
 
   return (
     <Wrapper>
@@ -60,19 +37,9 @@ export function Chat() {
       </ChatBox>
       <InputBox
         value={message}
-        onKeyPress={(e) => {
-          if(e.code === 'Enter' && e.ctrlKey) {
-            const newChats = [...chats];
-            newChats.push(message.replace(/^\s+/, '').replace(/\s+$/, ''));
-            setChats(newChats);
-            setMessage('');
-            e.preventDefault();
-          }
-        }}
-        onChange={(e) => {
-          // console.log(e.target.innerText);
-          setMessage(e.target.value);
-        }}/>
+        onKeyPress={keyPressHandler}
+        onChange={changeHandler}
+        />
     </Wrapper>
   );
 }
